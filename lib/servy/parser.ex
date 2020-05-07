3ab42acc7ab7222e13a6cd637b6a8b77f9001dfe
Alex Railean @xlippery <a.railean@dekart.com>
2020-05-07 22:45:54 +0200

    Add logic to actually parse headers
    We rely on recursion to process each line
    in the header, accumulating the results in
    a map, which is returned in the end.

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -7,7 +7,7 @@ defmodule Servy.Parser do
 	def parse(request) do
 		[header, raw_params] = String.split(request, "\n\n")
 
-		[request_line | _headers] = String.split(header, "\n")
+		[request_line | raw_headers] = String.split(header, "\n")
 
 	    [method, url, _version] = String.split(request_line, " ")
 	    	# header
@@ -17,15 +17,34 @@ defmodule Servy.Parser do
 
 	    params = parse_params(raw_params)
 
+	    headers = parse_headers(raw_headers, %{})
+
 	    # We treat this as a struct, not as a regular map,
 	    # note that we don't provide values for all elements,
 	    # because they're already defined in the struct itself.
 	    %Conv{method: method, path: url,
-	          params: params
+	          params: params,
+	          headers: headers
 	}
 	end
 
 	def parse_params(params) do
 		params |> String.trim |> URI.decode_query
 	end
+
+	def parse_headers([head | tail], accumulator) do
+		# IO.puts "Head: #{inspect(head)}, Tail: #{inspect(tail)}"
+
+		[key, value ] = head |> String.split(": ")
+		# IO.puts "K: #{inspect(key)}, V: #{inspect(value)}"
+
+		accumulator = Map.put(accumulator, key, value)
+		IO.inspect accumulator
+		parse_headers(tail, accumulator)
+		# result = %{}
+
+		# params |> String.trim |> URI.decode_query
+	end
+
+	def parse_headers([], accumulator), do: accumulator
 end
