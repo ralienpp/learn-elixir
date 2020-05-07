5a23bf50a30b2ec81478ca98930017f094b85805
Alex Railean @xlippery <a.railean@dekart.com>
2020-05-07 15:02:20 +0200

    Use a structure instead of a regular map

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -5,6 +5,10 @@ defmodule Servy.Parser do
 	    	|> String.split("\n")
 	    	|> List.first
 	    	|> String.split(" ")
-		%{method: method, path: url, resp_body: "", status: nil}
+
+	    # We treat this as a struct, not as a regular map,
+	    # note that we don't provide values for all elements,
+	    # because they're already defined in the struct itself.
+	    %Servy.Conv{method: method, path: url}
 	end
 end
